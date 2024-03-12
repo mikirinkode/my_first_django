7d455dbdad7e6db92a17be08ebb0e8892b865764
Muhammad Wafa <muhammadwafa105@gmail.com>
2024-03-12 14:49:51 +0700

    Feat: add member detail

--- members/views.py
+++ members/views.py
@@ -2,8 +2,24 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from django.template import loader
 
+from members.models import Member
+
 # Create your views here.
 def members(request):
     # return HttpResponse("Hello World!")
-    template = loader.get_template('myfirst.html')
-    return HttpResponse(template.render())
+    # template = loader.get_template('myfirst.html')
+    # return HttpResponse(template.render())
+    mymembers = Member.objects.all().values()
+    template = loader.get_template('all_members.html')
+    context = {
+        "mymembers": mymembers
+    }
+    return HttpResponse(template.render(context, request))
+
+def details(request, id):
+    mymember = Member.objects.get(id=id)
+    template = loader.get_template('details.html')
+    context = {
+        "mymember": mymember,
+    }
+    return HttpResponse(template.render(context, request))
